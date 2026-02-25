96305d507efc7c6738a091bea0050ba43de9cd14
Akash <akash300383@gmail.com>
2026-02-25 17:30:19 +0530

    done

---
+++ React JS Project/react-vite-testing-app/src/Hello.test.jsx
@@ -0,0 +1,17 @@
+import { describe } from "vitest";
+import Hello from "./Hello";
+import { render, screen } from "@testing-library/react";
+
+
+
+describe("Hello Component testing ",()=> {
+
+    test("should render Hello World with Vite! ", () => {
+        render(<Hello />);  // load the Hello Component 
+        // using screen to find the element with text "Hello World with Vite!" and check if it is in the document
+        const helloElement = screen.getByText(/Hello World with Vite!/i);
+        
+        expect(helloElement).toBeInTheDocument();
+    });
+})
+
